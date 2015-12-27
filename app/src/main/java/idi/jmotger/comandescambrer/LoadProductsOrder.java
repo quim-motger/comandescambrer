53534e006852e121162af563cf837234126f95fb
quim-motger <joaquim.motger@est.fib.upc.edu>
2015-12-27 17:08:41 +0100

    change

--- app/src/main/java/idi/jmotger/comandescambrer/LoadProductsOrder.java
+++ app/src/main/java/idi/jmotger/comandescambrer/LoadProductsOrder.java
@@ -1,17 +1,41 @@
 package idi.jmotger.comandescambrer;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import idi.jmotger.comandescambrer.idi.jmotger.comandescambrer.database.DataBaseSQLite;
 
 public class LoadProductsOrder extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         String type = getIntent().getExtras().getString("TYPE");
+
+        DataBaseSQLite d = new DataBaseSQLite(this);
+        SQLiteDatabase db = d.getReadableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM PRODUCT WHERE TYPE = ?", new String[]{type});
+
+        while (c.moveToNext()) {
+            Log.d("LOAD PRODUCTS", "Loading another product");
+            byte[] image = c.getBlob(3);
+            Bitmap bmp = BitmapFactory.decodeByteArray(image, 0, image.length);
+            ImageView v = (ImageView)findViewById(R.id.imageV);
+            v.setImageBitmap(bmp);
+
+        }
+
         switch(type) {
             case "FIRST":
                 setTitle("Nova comanda - Primers plats");
--- app/src/main/java/idi/jmotger/comandescambrer/idi/jmotger/comandescambrer/database/DataBaseSQLite.java
+++ app/src/main/java/idi/jmotger/comandescambrer/idi/jmotger/comandescambrer/database/DataBaseSQLite.java
@@ -1,10 +1,15 @@
 package idi.jmotger.comandescambrer.idi.jmotger.comandescambrer.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Created by jmotger on 27/12/15.
@@ -18,16 +23,33 @@ public class DataBaseSQLite extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        db.execSQL("CREATE TABLE ORDERS (ORDER_ID TEXT PRIMARY KEY, DATE TEXT, TIME TEXT, N_TABLE INTEGER);");
+        db.execSQL("CREATE TABLE IF NOT EXISTS ORDERS (ORDER_ID TEXT PRIMARY KEY, DATE TEXT, TIME TEXT, N_TABLE INTEGER);");
         db.execSQL("CREATE TABLE IF NOT EXISTS PRODUCT (NAME TEXT, PRICE REAL, TYPE TEXT, IMAGE BLOB, PRIMARY KEY (NAME));");
         db.execSQL("CREATE TABLE IF NOT EXISTS LINE_ORDER (ORDER_ID INTEGER, PRODUCT_NAME TEXT, QTT INTEGER, PRIMARY KEY (ORDER_ID, PRODUCT_NAME) FOREIGN KEY (ORDER_ID) REFERENCES ORDERS (ORDER_ID));");
 
-        initDatabase();
-    }
-
-    private void initDatabase() {
         //Init products
-        FileInputStream f = new FileInputStream("res/mipmap/");
+        try {
+            Log.d("DB", "Inserting first product");
+            FileInputStream f = new FileInputStream("agua.jpg");
+            byte[] aigua = new byte[f.available()];
+            f.read(aigua);
+            Log.d("DB", "Image loaded");
+
+            ContentValues values = new ContentValues();
+            values.put("NAME", "Aigua");
+            values.put("PRICE", "1.0");
+            values.put("TYPE", "DRINK");
+            values.put("IMAGE", aigua);
+
+            db.insert("PRODUCT", null, values);
+            Log.d("DB", "Inserted in database");
+            f.close();
+
+        }
+        catch (IOException e) {
+            Log.e("DB", "Error initiating database");
+        }
+
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
