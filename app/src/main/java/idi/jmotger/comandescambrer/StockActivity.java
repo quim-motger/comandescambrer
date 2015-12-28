fb3584d17639a690b1fc518057f05f96b9a79d02
quim-motger <joaquim.motger@est.fib.upc.edu>
2015-12-28 19:06:39 +0100

    changed dynamic of creating product again

--- app/src/main/java/idi/jmotger/comandescambrer/StockActivity.java
+++ app/src/main/java/idi/jmotger/comandescambrer/StockActivity.java
@@ -24,6 +24,15 @@ public class StockActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), NewProductActivity.class);
+                startActivity(intent);
+            }
+        });
+
         adap = new ButtonTypeAdapter(getApplicationContext());
         GridView gridView = (GridView) findViewById(R.id.gridButtonsStock);
         gridView.setAdapter(adap);
