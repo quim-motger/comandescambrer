e246a182281c270247fdea7839b40e0336f94370
quim-motger <quim.motger@gmail.com>
2016-01-04 20:27:41 +0100

    Last revision changes

--- app/src/main/java/idi/jmotger/comandescambrer/MainActivity.java
+++ app/src/main/java/idi/jmotger/comandescambrer/MainActivity.java
@@ -82,12 +82,8 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        startActivity(new Intent(getApplicationContext(), AboutActivity.class));
 
         return super.onOptionsItemSelected(item);
     }
